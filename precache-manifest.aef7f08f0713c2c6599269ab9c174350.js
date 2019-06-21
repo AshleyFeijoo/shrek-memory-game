0297ad1fe1b300a0cbb7cf0b9c7556030f3a09b1
Ash Feijoo <AshleyFeijoo1@gmail.com>
2019-06-21 12:48:40 -0500

    Updates

---
+++ precache-manifest.aef7f08f0713c2c6599269ab9c174350.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4a771f214171892b1cebf6c24bc539e",
+    "url": "/shrek-memory-game/index.html"
+  },
+  {
+    "revision": "63b7fec6bbb2c9e77bfb",
+    "url": "/shrek-memory-game/static/css/main.f8bb9b6a.chunk.css"
+  },
+  {
+    "revision": "372b633e91d3d3db7947",
+    "url": "/shrek-memory-game/static/js/2.dab05505.chunk.js"
+  },
+  {
+    "revision": "63b7fec6bbb2c9e77bfb",
+    "url": "/shrek-memory-game/static/js/main.5a6bdd5b.chunk.js"
+  },
+  {
+    "revision": "0a1ba9359964cd3a1555",
+    "url": "/shrek-memory-game/static/js/runtime~main.d33b789d.js"
+  },
+  {
+    "revision": "7e40b1c99603496a051e662c04b8f6c5",
+    "url": "/shrek-memory-game/static/media/CuShj4j.7e40b1c9.jpg"
+  },
+  {
+    "revision": "8ce4c3fa166ccaafa6a06bd30e6c3497",
+    "url": "/shrek-memory-game/static/media/Fergus.8ce4c3fa.jpeg"
+  },
+  {
+    "revision": "0088e1a8ebf24d634c3a8ef9dedd41ed",
+    "url": "/shrek-memory-game/static/media/Fiona.0088e1a8.jpeg"
+  },
+  {
+    "revision": "9c6b829116ce69c3c30a90010bc5c04e",
+    "url": "/shrek-memory-game/static/media/LordFarquad.9c6b8291.jpeg"
+  },
+  {
+    "revision": "64f8ddd9e8b6162ea218bb1179b8b4ec",
+    "url": "/shrek-memory-game/static/media/Pinnochio.64f8ddd9.png"
+  },
+  {
+    "revision": "d5ad5f8c23de195e7ce5b9048c7eecd1",
+    "url": "/shrek-memory-game/static/media/PrinceCharming.d5ad5f8c.jpeg"
+  },
+  {
+    "revision": "1a50c396689602d8f014345101e8184b",
+    "url": "/shrek-memory-game/static/media/PussInBoots.1a50c396.jpeg"
+  },
+  {
+    "revision": "2179845ee578cd3bb30b55dc922bfb38",
+    "url": "/shrek-memory-game/static/media/Shrek.2179845e.ttf"
+  },
+  {
+    "revision": "b0896ef3524d016879e93da9e0cc920a",
+    "url": "/shrek-memory-game/static/media/Shrek.b0896ef3.jpeg"
+  },
+  {
+    "revision": "602f2f9a4f9aacf08a84897ef8aae334",
+    "url": "/shrek-memory-game/static/media/ShrekMemory.602f2f9a.png"
+  },
+  {
+    "revision": "a7c190543d3e8ebc8a67d5aa4c4811ab",
+    "url": "/shrek-memory-game/static/media/desc.a7c19054.png"
+  },
+  {
+    "revision": "e3c29f19995d05aecb469dabb2741a50",
+    "url": "/shrek-memory-game/static/media/donkey.e3c29f19.jpeg"
+  },
+  {
+    "revision": "66401176dfd0e53cf16221becbd624e6",
+    "url": "/shrek-memory-game/static/media/humanShrek.66401176.jpg"
+  },
+  {
+    "revision": "8d5ee2f7942fb326e62b15bb499611ce",
+    "url": "/shrek-memory-game/static/media/shrekHeader.8d5ee2f7.png"
+  }
+]);
